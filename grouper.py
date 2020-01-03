7f011a1a189af57e321a731edf0aa410bb8b2534
polojerry <polojerry4@gmail.com>
2020-01-03 13:41:36 +0300

    Divide members into groups of five

--- grouper.py
+++ grouper.py
@@ -1,5 +1,7 @@
 import csv
 
+list_of_members = []
+
 
 def csv_reader(csv_file):
     file_reader = csv.reader(open(csv_file, newline=''))
@@ -8,13 +10,42 @@ def csv_reader(csv_file):
 
 def divide_multiple_selection(reader_file, area_of_interest, member_count=0):
     for member_row in reader_file:
-        member_selections = member_row[4]
-        member_selection = member_selections.split(";")
+        member_selection = member_row[4].split(";")
 
         for selection in member_selection:
             if selection == area_of_interest:
-                member_count += 1
-                print(area_of_interest + ": " + str(member_count))
+                list_of_members.append(member_row)
+
+
+def create_groups_file(area_of_interest):
+    return open(area_of_interest + " Groups.txt", "w+")
+
+
+def divide_list_into_groups(members_list, group_sizes):
+    for member in range(0, len(members_list), group_sizes):
+        yield list_of_members[member:member + group_sizes]
+
+
+def group_members(department_name, members_list, group_size):
+    group_file = create_groups_file(department_name)
+    members_groups = divide_list_into_groups(members_list, group_size)
+
+    group_counter = 1
+    for member_group in members_groups:
+        student_counter = 1
+        group_file.write("Group " + str(group_counter) + "\n")
+
+        for member in member_group:
+            group_file.write(
+                str(student_counter) + ". " + member[1] +
+                " ||" + " Contact: " + member[2] +
+                " || " + " Level: " + member[5] +
+                " || " + " Year of Study: " + member[3] +
+                "\n")
+            student_counter += 1
+
+        group_file.write("\n")
+        group_counter += 1
 
 
 if __name__ == "__main__":
@@ -22,4 +53,5 @@ if __name__ == "__main__":
     reader = csv_reader(csv_file_path)
     sorted_list = sorted(reader, key=lambda _row: _row[4], reverse=False)
 
-    divide_multiple_selection(sorted_list, "Android Development")
+    divide_multiple_selection(sorted_list, "Web Development")
+    group_members("Android Development", list_of_members, 6)
